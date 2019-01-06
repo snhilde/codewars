c26329fd023cd36a8d3f8ffcf03e931fc67b741f
Hilde N <snhilde@protonmail.com>
2019-01-06 18:56:04 -0500

    solution

--- n_divisible.c
+++ n_divisible.c
@@ -11,6 +11,14 @@
  * 		isDivisible(100, 5, 3) -> false because 100 is not divisible by 3
  * 		isDivisible(12, 7, 5) -> false because 12 is neither divisible by 7 nor 5 */
 
+static void
+isDivisible(int n, int x, int y)
+{
+	printf("---\n");
+	printf("n: %i\nx: %i\ny: %i\n", n, x, y);
+	printf("%s\n", (!(n % x) && !(n % y)) ? "true" : "false");
+}
+
 int
 main(int argc, char *argv[])
 {
