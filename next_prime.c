432123583cb483b4a87bd6f2320c11c9b0bf7fe5
Hilde N <snhilde@protonmail.com>
2019-01-06 19:17:08 -0500

    initialize next_prime

---
+++ next_prime.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+/* TITLE: Next Prime
+ * AUTHOR: zruF
+ * KYU: 7
+ * 
+ * Get the next prime number!
+ * 
+ * You will get a number n (>=0) and your task is to find the next prime number.
+ * 
+ * Make sure to optimize your code; there will be numbers tested up to about 10^12.
+ * 
+ * Examples
+ * 5 ==> 7
+ * 12 ==> 13
+*/
+
+int
+main(int argc, char *argv[])
+{
+	
+	
+	return 0;
+}
