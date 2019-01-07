16d4f2dcb1cfd7f37ea8b0371758b5ee1470a2fa
Hilde N <snhilde@protonmail.com>
2019-01-06 19:14:26 -0500

    initialize narc_nums

---
+++ narc_nums.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+/* TITLE: Narcissistic Numbers
+ * AUTHOR: Staples
+ * KYU: 7
+ * 
+ * A Narcissistic Number is a number of length n in which the sum of its digits
+ * to the power of n is equal to the original number. If this seems confusing,
+ * refer to the example below.
+ * 
+ * Ex. 153, where n=3 (number of digits in 153)
+ * 
+ * 1^3 + 5^3 + 3^3 = 153
+ * 
+ * Write a method is_narcissistic(i) which returns whether or not i is a
+ * Narcissistic Number.
+*/
+
+static bool
+is_narcissistic(int i)
+{
+}
+
+int
+main(int argc, char *argv[])
+{
+	
+	
+	return 0;
+}
